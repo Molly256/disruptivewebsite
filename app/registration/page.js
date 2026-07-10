d22335e12b03b5b375017b13e934e6320631a384
maureennakiyingi <maureennakiyingi50@gmail.com>
2026-07-11 00:17:49 +0300

    Add registration page + floating GET STARTED button

--- app/registration/page.js
+++ app/registration/page.js
@@ -1,3 +1,14 @@
 export default function Registration() {
-  return <div style={{padding:'100px 20px'}}>Registration Page</div>
+  return (
+    <div style={{
+      padding:'120px 20px',
+      minHeight:'100vh',
+      background:'#fff',
+      color:'#000',
+      textAlign:'center'
+    }}>
+      <h1>Registration</h1>
+      <p>Form coming soon</p>
+    </div>
+  )
 }
