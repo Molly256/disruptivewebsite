de49ca4a80060bd57c590373f8077d06cece9290
maureennakiyingi <maureennakiyingi50@gmail.com>
2026-07-11 21:57:12 +0300

    Add favicon for browser tab

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,10 @@ import ChatWidget from '../components/ChatWidget'
 export const metadata = {
   title: 'Disruptive - Digital Marketing That Actually Works',
   description: 'Paid Search, Paid Social, SEO, Amazon & more. We grow brands that scale.',
+  icons: {
+    icon: '/favicon.png',
+    apple: '/favicon.png', // for iOS home screen
+  },
 }
 
 export default function RootLayout({ children }) {
