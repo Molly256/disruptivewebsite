f0c470506175a084fe5946b30582364dabc015f5
maureennakiyingi <maureennakiyingi50@gmail.com>
2026-07-12 01:14:40 +0300

    fix: add missing useRef import to ChatWidget

--- components/ChatWidget.js
+++ components/ChatWidget.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useRouter, usePathname } from 'next/navigation'
 
 export default function ChatWidget() {
