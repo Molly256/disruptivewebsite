acc46cac6a6d32f137f4277bf9f180d8999445fc
maureennakiyingi <maureennakiyingi50@gmail.com>
2026-09-02 16:29:07 +0300

    fix x10 combo profit for vip1 day2 set3 id38 and others

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,4 +1,11 @@
 export const metadata = {
   manifest: "/admin-manifest.json",
-  themeColor: "#00a884"
+}
+
+export const viewport = {
+  themeColor: "#00a884",
+}
+
+export default function AdminLayout({ children }) {
+  return children
 }
